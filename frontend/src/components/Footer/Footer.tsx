3f1fb9543958cf63dec0e07ba11304a052769261
Derrick <pshubomi@gmail.com>
2024-10-07 01:13:12 +0100

    changed the social icons and add links to them

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,32 +1,45 @@
 import Image from "next/image";
+import Link from "next/link";
 export default function Footer(){
     return(
               <footer className="bg-[#293857] h-[150px] flex items-center justify-center flex-col text-white">
         <div className="flex gap-4 items-center mb-5">
+          <Link href="https://www.facebook.com/profile.php?id=61565977402117&mibextid=ZbWKwL" target="_blank">
+            <Image
+              src="/images/Facebook.png"
+              alt="social icon"
+              height="40"
+              width="40"
+              className="scale-150"
+            />
+          </Link>
+        <Link href="https://www.instagram.com/sleekyprogrammers?igsh=c2x4cTU1eGY5aGdj" target="_blank">
           <Image
-            src="images/social.svg"
+            src="/images/_Instagram.png"
             alt="social icon"
             height="40"
             width="40"
+            className="scale-150"
           />
+        </Link>
+          <Link href="https://www.linkedin.com/company/sleeky-programmers-limited/" target="_blank">
+            <Image
+              src="/images/skill-icons_linkedin.png"
+              alt="social icon"
+              height="40"
+              width="40"
+              className="scale-150"
+            />
+          </Link>
+          <Link href="https://x.com/sleekydevs?s=09" target="_blank">
           <Image
-            src="images/social(1).svg"
+            src="/images/_Figma.png"
             alt="social icon"
             height="40"
             width="40"
+            className="scale-150"
           />
-          <Image
-            src="images/social(2).svg"
-            alt="social icon"
-            height="40"
-            width="40"
-          />
-          <Image
-            src="images/social(3).svg"
-            alt="social icon"
-            height="40"
-            width="40"
-          />
+        </Link>
         </div>
         <p>© 2024 Copyright. ChurchWebflow.com</p>
       </footer>
